a9e9384df9e2380563221eb8859a45d808369352
Emil Dmitruk <e.dmitruk@herts.ac.uk>
2020-09-10 17:49:03 +0200

    Refactored

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,5 @@
 using SafeTestsets
 
-
-
-
 @safetestset "MatrixProcessing tests" begin include("matrixProcessing_tests.jl") end
 
 @safetestset "MatrixOrganization tests" begin include("matrixOrganisation_tests.jl") end
