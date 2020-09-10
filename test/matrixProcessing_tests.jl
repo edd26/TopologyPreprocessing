34487d3d94f03f3ce0b3337e7ee565008a8c3fa7
Emil Dmitruk <e.dmitruk@herts.ac.uk>
2020-09-10 17:48:52 +0200

    Tests for distances and indices

--- test/matrixProcessing_tests.jl
+++ test/matrixProcessing_tests.jl
@@ -4,7 +4,7 @@ using LinearAlgebra
 
 # using MatrixOrganization
 
-@testset "MatrixProcessing.jl -> " begin
+@testset "MatrixProcessing.jl -> basics" begin
     # Ints
     positive_matrix = [1 2 3;
                        4 5 6]
@@ -56,9 +56,83 @@ using LinearAlgebra
 
     @test diagonal_symmetrize(square_matrix, below_over_upper=true)[1,end] == square_matrix[end,1]
     @test diagonal_symmetrize(square_matrix, below_over_upper=true)[1,2] == square_matrix[2,1]
+end
 
-    # =====================
+@testset "MatrixProcessing.jl -> distances and indices" begin
+    # Ints
+    positive_matrix = [1 2 3;4 5 6]
+    positive_matrix2 = [1. 2 3; 4 5 6]
+    positive_3d_matrix = rand(2,3,4)
 
-    
+    negative_matrix = [1 2 -3;-4 5 6]
+    negative_matrix2 = [1 2 -3; -4 5 6]
+    negative_3d_matrix = rand(2,3,4).*2 .-1
+
+    negative_6d_matrix = rand(2,3,4,5,6,7).*2 .-1
+
+    powers_of_2 = [0 ; [2^k for k in 0:2:8]]
+    powers_of_3 = [0 ; [3^k for k in 0:2:8]]
+
+    square_matrix = [1 2 3;
+                     4 5 6;
+                     7 8 9]
+
+    # ========================================================
+
+    @test length(unique(group_distances(square_matrix,1))) == 1
+    @test length(unique(group_distances(square_matrix,2))) == 2
+    @test length(unique(group_distances(square_matrix,9))) == 9
+    @test_throws DomainError group_distances(square_matrix,20)
+
+    @test length(unique(group_distances(positive_matrix,1))) == 1
+    @test length(unique(group_distances(positive_matrix,2))) == 2
+    @test length(unique(group_distances(positive_matrix,6))) == 6
+    @test_throws DomainError group_distances(positive_matrix,20)
+
+    @test length(unique(group_distances(positive_3d_matrix,2))) == 2
+    @test length(unique(group_distances(negative_3d_matrix,2))) == 2
+    @test length(unique(group_distances(negative_6d_matrix,2))) == 2
+
+    group_distances(square_matrix,2)
+
+    # ========================================================
+
+    @test length(generate_indices(3))==3^2
+    @test length(generate_indices(223))==223^2
+
+    n=3
+    @test length(generate_indices(n, symmetry_order=true, include_diagonal=false)) == ((n^2 - n) ÷ 2)
+    n=9
+    @test length(generate_indices(n, symmetry_order=true, include_diagonal=false)) == ((n^2 - n) ÷ 2)
+    index_matrix1 = generate_indices(n, symmetry_order=true, include_diagonal=false)
+    @test length(findall(x-> x == CartesianIndex(n,n),index_matrix1)) == 0
+    @test length(findall(x-> x == CartesianIndex(n-2,n-1),index_matrix1)) == 1
+    @test length(findall(x-> x == CartesianIndex(n-1,n-2),index_matrix1)) == 0
+
+    n=223
+    @test length(generate_indices(n, symmetry_order=true)) == ((n^2 - n) ÷ 2)
+
+    n=3
+    index_matrix2 = generate_indices(n, symmetry_order=true, include_diagonal=true)
+    @test length(index_matrix2) == (((n-1)*n)÷2 +n)
+    @test findmax(index_matrix2)[1] == CartesianIndex(n,n)
+    @test length(findall(x-> x == CartesianIndex(1,n),index_matrix2)) == 1
+    @test length(findall(x-> x == CartesianIndex(1,1),index_matrix2)) == 1
+    @test length(findall(x-> x == CartesianIndex(n,n),index_matrix2)) == 1
+
+    n=9
+    @test length(generate_indices(n, symmetry_order=true, include_diagonal=true)) == (((n-1)*n)÷2 +n)
+    n=223
+    @test length(generate_indices(n, symmetry_order=true, include_diagonal=true)) == (((n-1)*n)÷2 +n)
+
+
+end
+
+
+@testset "MatrixProcessing.jl -> arrays of arrays" begin
+
+end
+
+@testset "MatrixProcessing.jl -> matrix ordering" begin
 
 end
