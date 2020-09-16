163f2e09d5d7a22177686daece15cde8f27e8214
Emil Dmitruk <e.dmitruk@herts.ac.uk>
2020-09-16 17:11:48 +0200

    Included BettiCurves tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,5 @@ using SafeTestsets
 @safetestset "MatrixProcessing tests" begin include("matrixProcessing_tests.jl") end
 
 @safetestset "MatrixOrganization tests" begin include("matrixOrganisation_tests.jl") end
+
+@safetestset "BettiCurves tests" begin include("bettiCurves_tests.jl") end
