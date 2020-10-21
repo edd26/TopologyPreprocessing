132bb1ec4b8920036fa8fa38d3eb285e03731ab4
Emil Dmitruk <e.dmitruk@herts.ac.uk>
2020-10-21 14:40:58 +0200

    Changed the includes

--- src/TopologyPreprocessing.jl
+++ src/TopologyPreprocessing.jl
@@ -1,21 +1,48 @@
 module TopologyPreprocessing
 
-include("AverageBettis.jl")
-include("BettiCurves.jl")
-include("DirOperations.jl")
-include("GeometricSampling.jl")
-include("GifGenerator.jl")
-include("ImageProcessing.jl")
-include("MatrixOrganization.jl")
 include("MatrixProcessing.jl")
-include("MatrixToolbox.jl")
-include("PlottingWrappers.jl")
-include("PointsSubstitution.jl")
-include("SavingFigures.jl")
-include("VideoProcessing.jl")
+include("MatrixOrganization.jl")
+include("BettiCurves.jl")
+
+# include("AverageBettis.jl")
+
+# include("DirOperations.jl")
+# include("GeometricSampling.jl")
+# include("GifGenerator.jl")
+# include("ImageProcessing.jl")
+#
+#
+# include("MatrixToolbox.jl")
+# include("PlottingWrappers.jl")
+# include("PointsSubstitution.jl")
+# include("SavingFigures.jl")
+# include("VideoProcessing.jl")
 
 export matrix_poling,
         subsample_matrix,
         add_random_patch
+
+export shift_to_non_negative,
+        normalize_to_01,
+        diagonal_symmetrize,
+        group_distances,
+        generate_indices,
+        reduce_arrs_to_min_len,
+        reduce_arrs_to_max_len
+
+export get_ordered_matrix,
+        group_distances,
+        generate_indices,
+        arr_to_vec,
+        cartesianInd_to_vec,
+        sort_index_by_values,
+        set_values!
+
+export get_bettis,
+        normalise_bettis,
+        get_vectorized_bettis,
+        plot_bettis
+
+
 
 end # module
