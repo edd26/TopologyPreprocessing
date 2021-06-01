2d23c3630f6a0cbbf5e230edbf970da238bee96c
edd26 <e.dmitruk@herts.ac.uk>
2021-06-01 13:45:21 +0200

    Fixed dimensions issue; added TODO

--- src/BettiCurves.jl
+++ src/BettiCurves.jl
@@ -4,8 +4,8 @@ using Eirene
 using Plots
 using PlotThemes
 using PlotUtils
-using Dierckx
 using StatsPlots
+# using Dierckx
 
 #%%
 function get_bettis(results_eirene::Dict, max_dim::Integer; min_dim::Int = 1)
@@ -172,12 +172,13 @@ function plot_bettis(bettis::Vector;
     colors_set = get_bettis_color_palete(min_dim=min_dim)
     plot_ref = plot(; kwargs...)
     # for p = min_dim:(max_dim) #TODO ths can not be starting from min_dim, because it may be 0
-    for p = all_iterations
-        args = (lc = colors_set[p], linewidth = lw)
+    # for p = all_iterations
+    for (index, p) in enumerate(min_dim:max_dim)
+        args = (lc = colors_set[index], linewidth = lw)
         if betti_labels
-            args = (args..., label = "β$(all_dims[p])")
+            args = (args..., label = "β$(p)")
         end
-        plot!(bettis[p][:, 1], bettis[p][:, 2]; args...)
+        plot!(bettis[index][:, 1], bettis[index][:, 2]; args...)
     end
 
     legend_pos = findfirst(x -> x == :legend, keys(kwargs))
@@ -237,7 +238,7 @@ function plot_bettis(bettis::Array;
     TODO: min_dim is not included in all_dims variable
     TODO: add change of x label based on x values- so it is either edge density for 0:1 range values or Filtration step otherwise
     """
-    max_dim = size(bettis, 2)
+    max_dim = size(bettis, 2)-1-min_dim
     all_dims = 1:max_dim
 
     if min_dim > max_dim
@@ -261,22 +262,24 @@ function plot_bettis(bettis::Array;
         lw = 2
     end
 
-    if use_edge_density
-        for p = 1:(max_dim) #TODO ths can not be starting from min_dim, because it may be 0
-            max_step = findmax(bettis[p][:, 1])[1]
-            bettis[p][:, 1] ./=max_step
-        end
-    end
+    # if use_edge_density
+    #     # for p = 1:(max_dim) #TODO ths can not be starting from min_dim, because it may be 0
+    #     for (index, p) in enumerate(min_dim:max_dim)
+    #         max_step = findmax(bettis[:, 1])[1]
+    #         bettis[p][:, 1] ./=max_step
+    #     end
+    # end
 
     colors_set = get_bettis_color_palete(min_dim=min_dim)
     plot_ref = plot(; kwargs...)
     # for p = min_dim:(max_dim) #TODO ths can not be starting from min_dim, because it may be 0
-    for p = 1:(max_dim) #TODO ths can not be starting from min_dim, because it may be 0
-        args = (lc = colors_set[p], linewidth = lw)
+    # for p = 1:(max_dim) #TODO ths can not be starting from min_dim, because it may be 0
+    for (index, p) in enumerate(min_dim:max_dim)
+        args = (lc = colors_set[index], linewidth = lw)
         if betti_labels
-            args = (args..., label = "β$(all_dims[p])")
+            args = (args..., label = "β$(p)")
         end
-        plot!(x_vals, bettis[:, p]; args...)
+        plot!(x_vals, bettis[:, index]; args...)
     end
 
     legend_pos = findfirst(x -> x == :legend, keys(kwargs))
@@ -683,6 +686,7 @@ function get_bettis_from_image(img_name,
     # ==============================================================================
     # ================================ Plot results ================================
 
+# TODO separate plotting from processing
     if plot_heatmaps
 
         full_ordered_matrix = get_ordered_matrix(C_ij; assing_same_values = false)
