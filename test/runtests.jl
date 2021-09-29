bedb1d77ce444dc220a6be1caaaf59f52ff6a074
edd26 <e.dmitruk@herts.ac.uk>
2021-09-29 19:46:41 +0200

    Visual refactoring

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,19 @@
 using SafeTestsets
 # TODO add description to the tests
 
+## ===-===-===-===-===-===-===-===-
 @safetestset "MatrixProcessing tests" begin
     include("matrixProcessing_tests.jl")
 end
 
+## ===-===-===-===-===-===-===-===-
 @safetestset "MatrixOrganization tests" begin
     include("matrixOrganisation_tests.jl")
 end
 
+## ===-===-===-===-===-===-===-===-
 @safetestset "BettiCurves tests" begin
     include("bettiCurves_tests.jl")
 end
+
+## ===-===-===-===-===-===-===-===-
