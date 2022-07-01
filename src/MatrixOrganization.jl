0e74d036fe655f37ffdac74d5faa5cbbf283236a
ed19aaf <e.dmitruk@herts.ac.uk>
2022-07-01 10:41:41 +0100

    Slpit some functions from the plotting

--- src/MatrixOrganization.jl
+++ src/MatrixOrganization.jl
@@ -7,28 +7,33 @@ include("PlottingWrappers.jl")
 include("PointsSubstitution.jl")
 
 """
-	function expand_matrix(input_matrix, expansion_size, last_components;do_plot=false)
+	function expand_matrix(input_matrix, expansion_size, last_components
 
 Takes 'input_matrix' (an ordering matrix used for creating cliques) and and adds
 2×'expansion_size' number of rows. 'last_components' are the values in original
 matrix that are added last to the clique.
 
-Results may be be plotted by setting 'do_plot=true'.
+Results may be be plotted with same funtion with sufix "with_plot".
 """
-function expand_matrix(input_matrix, expansion_size, last_components;do_plot=false)
-	new_comp = last_components
-	matrix_size = size(input_matrix,1)
-	for mat_sizes = matrix_size:2:(matrix_size+2expansion_size)
-		input_matrix, new_comp = add_step_to_matrix(input_matrix, new_comp)
-	end
-	if do_plot
-    # TODO separate plotting from processing
-		expand_plt_ref = plot_square_heatmap(input_matrix, 1,size(input_matrix,1);
-											plt_title = "Original, size:$(matrix_size)",
-											color_palete=:lightrainbow)
-		display(expand_plt_ref)
-	end
-	return input_matrix, expand_plt_ref
+function expand_matrix(input_matrix, expansion_size, last_components)
+    new_comp = last_components
+    matrix_size = size(input_matrix, 1)
+    for mat_sizes = matrix_size:2:(matrix_size+2expansion_size)
+        input_matrix, new_comp = add_step_to_matrix(input_matrix, new_comp)
+    end
+
+    return input_matrix
+end
+
+function expand_matrix_with_plot(args...)
+    input_matrix = expand_matrix_with_plot(args...)
+
+    expand_plt_ref = plot_square_heatmap(input_matrix, 1, size(input_matrix, 1);
+        plt_title="Original, size:$(matrix_size)",
+        color_palete=:lightrainbow)
+    display(expand_plt_ref)
+
+    return input_matrix, expand_plt_ref
 end
 
 # Shuffle matrix entries
@@ -39,26 +44,30 @@ Takes symmetric 'input_matrix' and randomly swaps rows 'shuffles' many times.
 
 Results may be plotted by setting 'do_plot=true'.
 """
+function shuffle_matrix(input_matrix, total_shuffles)
+    matrix_size = size(input_matrix, 1)
+    rows = randcycle(matrix_size)
+    shuffled_ord_mat = copy(input_matrix)
 
-function shuffle_matrix(input_matrix, shuffles; do_plot=false)
-	matrix_size = size(input_matrix,1)
-	rows = randcycle(matrix_size)
-	shuffled_ord_mat = copy(input_matrix)
+    for k = 1:total_shuffles
+        # global shuffled_ord_mat, rows
+        srcs, trgts = rand(rows, 2)
+        swap_rows!(shuffled_ord_mat, srcs, trgts)
+    end
 
-	for k = 1:shuffles
-		# global shuffled_ord_mat, rows
-		srcs, trgts = rand(rows,2)
-		swap_rows!(shuffled_ord_mat, srcs, trgts)
-	end
+    return shuffled_ord_mat
+end
 
-	if do_plot
-    # TODO separate plotting from processing
-		shuff_plt_ref = plot_square_heatmap(shuffled_ord_mat, 1,size(shuffled_ord_mat,1);
-											plt_title = "Shuffled, size:$(matrix_size)",
-											color_palete=:lightrainbow)
-		display(shuff_plt_ref)
-	end
-	return input_matrix, shuff_plt_ref
+function shuffle_matrix_with_plotting(args...)
+    shuffled_ord_mat = shuffle_matrix(args...)
+
+    if do_plot
+        shuff_plt_ref = plot_square_heatmap(shuffled_ord_mat, 1, size(shuffled_ord_mat, 1);
+            plt_title="Shuffled, size:$(matrix_size)",
+            color_palete=:lightrainbow)
+        display(shuff_plt_ref)
+    end
+    return input_matrix, shuff_plt_ref
 end
 
 
@@ -71,28 +80,31 @@ next to the diagonal.
 
 Results may be plotted by setting 'do_plot=true'.
 """
-function organize_shuff_matrix(input_matrix; do_plots=false)
-	unscrambled_matrix = copy(input_matrix)
-	matrix_size = size(input_matrix,1)
-	for k = matrix_size:-2:2
-		max_row_val = findmax(unscrambled_matrix[k,:])[2]
-		# put to the previous last position
-		swap_rows!(unscrambled_matrix, max_row_val, k-1)
-		# skip 1 row and work on next one
-	end
-	if do_plots
-    # TODO separate plotting from processing
-		reorganized_plt_ref = plot_square_heatmap(unscrambled_matrix, 1,size(unscrambled_matrix,1);
-									plt_title = "unscrambled_matrix, size:$(matrix_size)",
-									color_palete=:lightrainbow)
-		display(reorganized_plt_ref)
-		return unscrambled_matrix, reorganized_plt_ref
-	else
-		return unscrambled_matrix
-	end
+function organize_shuff_matrix(input_matrix)
+    unscrambled_matrix = copy(input_matrix)
+    matrix_size = size(input_matrix, 1)
+    for k = matrix_size:-2:2
+        max_row_val = findmax(unscrambled_matrix[k, :])[2]
+        # put to the previous last position
+        swap_rows!(unscrambled_matrix, max_row_val, k - 1)
+        # skip 1 row and work on next one
+    end
+
+    return unscrambled_matrix
 end
 
+function organize_shuff_matrix_with_plotting(input_matrix)
 
+    unscrambled_matrix = organize_shuff_matrix(input_matrix)
+
+    reorganized_plt_ref = plot_square_heatmap(unscrambled_matrix, 1, size(unscrambled_matrix, 1);
+        plt_title="unscrambled_matrix, size:$(matrix_size)",
+        color_palete=:lightrainbow
+    )
+    display(reorganized_plt_ref)
+
+    return unscrambled_matrix, reorganized_plt_ref
+end
 """
 		function order_max_vals_near_diagonal(input_matrix; do_plots=false, direction=:descending)
 
@@ -104,53 +116,55 @@ TODO- not working-  Optionally, ascending order can be used by setting 'directio
 
 Results may be plotted by setting 'do_plot=true'.
 """
-function order_max_vals_near_diagonal(input_matrix; do_plots=false, direction=:descending)
-	# Find max values next to the diagonal
-	matrix_size = size(input_matrix,1)
+function order_max_vals_near_diagonal(input_matrix; direction=:descending)
+    # Find max values next to the diagonal
+    matrix_size = size(input_matrix, 1)
 
-	if direction == :descending
-		# ordering_function = findmax
-		new_ord_value = -1
-		iteration_values = matrix_size:-2:2
-		# iteration_values = 2:2:matrix_size
+    if direction == :descending
+        # ordering_function = findmax
+        new_ord_value = -1
+        iteration_values = matrix_size:-2:2
+        # iteration_values = 2:2:matrix_size
 
-	elseif direction == :ascending
-		# ordering_function = findmin
-		new_ord_value = findmax(input_matrix)[1]*2
-		iteration_values = 2:2:matrix_size
-	else
-		# @error "Unknow ordering was given"
-		throw("Unknow ordering was given")
-	end
+    elseif direction == :ascending
+        # ordering_function = findmin
+        new_ord_value = findmax(input_matrix)[1] * 2
+        iteration_values = 2:2:matrix_size
+    else
+        # @error "Unknow ordering was given"
+        throw("Unknow ordering was given")
+    end
 
-	reordered_matrix = copy(input_matrix)
-	row_indices = 1:2:matrix_size
-	col_indices = 2:2:matrix_size
-	coord_set = [CartesianIndex(row_indices[k], col_indices[k]) for k=1:matrix_size÷2]
-	diag_max_values = reordered_matrix[coord_set]
+    reordered_matrix = copy(input_matrix)
+    row_indices = 1:2:matrix_size
+    col_indices = 2:2:matrix_size
+    coord_set = [CartesianIndex(row_indices[k], col_indices[k]) for k = 1:matrix_size÷2]
+    diag_max_values = reordered_matrix[coord_set]
 
-	for k = iteration_values
-		max_val, max_ind = findmax(diag_max_values)
-		(direction == :descending) ? (position = floor(k÷2)) : (position = floor(k÷2))
-		diag_max_values[max_ind] = diag_max_values[position]
-		diag_max_values[position] = new_ord_value
-		max_ind *= 2
+    for k = iteration_values
+        max_val, max_ind = findmax(diag_max_values)
+        (direction == :descending) ? (position = floor(k ÷ 2)) : (position = floor(k ÷ 2))
+        diag_max_values[max_ind] = diag_max_values[position]
+        diag_max_values[position] = new_ord_value
+        max_ind *= 2
 
-		swap_rows!(reordered_matrix, k, max_ind)
-		swap_rows!(reordered_matrix, k-1, max_ind-1)
-	end
+        swap_rows!(reordered_matrix, k, max_ind)
+        swap_rows!(reordered_matrix, k - 1, max_ind - 1)
+    end
+
+    return reordered_matrix
+end
 
 
-	if do_plots
-    # TODO separate plotting from processing
-		reorganized_plt_ref = plot_square_heatmap(reordered_matrix, 1,size(reordered_matrix,1);
-									plt_title = "reordered_matrix, size:$(matrix_size)",
-									color_palete=:lightrainbow)
-		display(reorganized_plt_ref)
-		return reordered_matrix, reorganized_plt_ref
-	else
-		return reordered_matrix
-	end
+function order_max_vals_near_diagonal_with_plotting(input_matrix; kwargs...)
+    reordered_matrix = order_max_vals_near_diagonal(input_matrix; kwargs...)
+
+    reorganized_plt_ref = plot_square_heatmap(reordered_matrix, 1, size(reordered_matrix, 1);
+        plt_title="reordered_matrix, size:$(matrix_size)",
+        color_palete=:lightrainbow)
+    display(reorganized_plt_ref)
+
+    return reordered_matrix, reorganized_plt_ref
 end
 
 
@@ -159,43 +173,48 @@ end
 
 Check if velues next to the maximal values are organized in descending order.
 """
-function fine_tune_matrix(input_matrix; do_plots=false)#, direction=:descending)
-	# Find max values next to the diagonal
-	matrix_size = size(input_matrix,1)
-	fine_tune_matrix = copy(input_matrix)
+function fine_tune_matrix(input_matrix)
+    # Find max values next to the diagonal
+    matrix_size = size(input_matrix, 1)
+    fine_tune_matrix = copy(input_matrix)
 
-	# if direction == :descending
-	# 	# ordering_function = findmax
-	# 	new_ord_value = -1
-	# 	iteration_values = matrix_size:-2:2
-	# 	# iteration_values = 2:2:matrix_size
-	#
-	# elseif direction == :ascending
-	# 	# ordering_function = findmin
-	# 	new_ord_value = findmax(input_matrix)[1]*2
-	# 	iteration_values = 2:2:matrix_size
-	# else
-	# 	# @error "Unknow ordering was given"
-	# 	throw("Unknow ordering was given")
-	# end
-
-
-	for k = 2:2:matrix_size-1
-		if fine_tune_matrix[k-1,k+1] > fine_tune_matrix[k,k+1]
-			swap_rows!(fine_tune_matrix, k, k-1)
-		end
-	end
+    # if direction == :descending
+    # 	# ordering_function = findmax
+    # 	new_ord_value = -1
+    # 	iteration_values = matrix_size:-2:2
+    # 	# iteration_values = 2:2:matrix_size
+    #
+    # elseif direction == :ascending
+    # 	# ordering_function = findmin
+    # 	new_ord_value = findmax(input_matrix)[1]*2
+    # 	iteration_values = 2:2:matrix_size
+    # else
+    # 	# @error "Unknow ordering was given"
+    # 	throw("Unknow ordering was given")
+    # end
 
 
-	if do_plots
-    # TODO separate plotting from processing
-		fine_tuned_plt_ref = plot_square_heatmap(fine_tune_matrix, 1,size(reordered_matrix,1);
-										plt_title = "fine_tuned, size:$(matrix_size)",
-										color_palete=:lightrainbow)
-		display(fine_tuned_plt_ref)
-	end
-	return fine_tune_matrix, fine_tuned_plt_ref
+    for k = 2:2:matrix_size-1
+        if fine_tune_matrix[k-1, k+1] > fine_tune_matrix[k, k+1]
+            swap_rows!(fine_tune_matrix, k, k - 1)
+        end
+    end
+
+    return fine_tune_matrix
 end
+
+
+function fine_tune_matrix_with_ploting(input_matrix)
+    fine_tune_matrix = fine_tune_matrix(input_matrix)
+
+    fine_tuned_plt_ref = plot_square_heatmap(fine_tune_matrix, 1, size(reordered_matrix, 1);
+        plt_title="fine_tuned, size:$(matrix_size)",
+        color_palete=:lightrainbow)
+    display(fine_tuned_plt_ref)
+
+    return fine_tune_matrix, fine_tuned_plt_ref
+end
+
 
 # TODO separate plotting from processing
 function order_max_vals_by_row_avg(input_matrix; do_plots=false)
