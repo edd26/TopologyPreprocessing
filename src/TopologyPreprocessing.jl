2673e31521d4de05610dd0f37572612c5a47e55f
Emil Dmitruk <e.dmitruk@herts.ac.uk>
2025-06-26 13:44:41 +0100

    refactor(TopoProcess): applied formatter to the code

--- src/TopologyPreprocessing.jl
+++ src/TopologyPreprocessing.jl
@@ -1,48 +1,48 @@
 module TopologyPreprocessing
-    # MatrixOrganization.jl
-    export matrix_poling,
-            subsample_matrix,
-            add_random_patch
+# MatrixOrganization.jl
+export matrix_poling,
+    subsample_matrix,
+    add_random_patch
 
-    # MatrixProcessing.jl
-    export shift_to_non_negative,
-            normalize_to_01,
-            diagonal_symmetrize,
-            group_distances,
-            generate_indices,
-            reduce_arrs_to_min_len,
-            increase_arrs_to_max_len,
-            get_ordered_matrix,
-            group_distances,
-            generate_indices,
-            arr_to_vec,
-            cartesianInd_to_vec,
-            sort_indices_by_values,
-            set_values!
+# MatrixProcessing.jl
+export shift_to_non_negative,
+    normalize_to_01,
+    diagonal_symmetrize,
+    group_distances,
+    generate_indices,
+    reduce_arrs_to_min_len,
+    increase_arrs_to_max_len,
+    get_ordered_matrix,
+    group_distances,
+    generate_indices,
+    arr_to_vec,
+    cartesianInd_to_vec,
+    sort_indices_by_values,
+    set_values!
 
-    # BettiCurves.jl
-    export get_bettis,
-            normalise_bettis,
-            get_vectorized_bettis,
-            plot_bettis,
-            get_bettis_color_palete
+# BettiCurves.jl
+export get_bettis,
+    get_normalised_barcodes,
+    get_vectorized_bettis,
+    plot_bettis,
+    get_bettis_color_palete
 
-    # BarCodes.jl
-    export get_barcodes,
-            plot_barcodes,
-            plot_barcodes!,
-            get_birth_death_ratio,
-            get_barcode_lifetime,
-            get_barcode_max_lifetime,
-            boxplot_birth_death,
-            boxplot_lifetime,
-            get_barcode_max_db_ratios
+# BarCodes.jl
+export get_barcodes,
+    plot_barcodes,
+    plot_barcodes!,
+    get_birth_death_ratio,
+    get_barcode_lifetime,
+    get_barcode_max_lifetime,
+    boxplot_birth_death,
+    boxplot_lifetime,
+    get_barcode_max_db_ratios
 
 
-    include("MatrixOrganization.jl")
-    include("MatrixProcessing.jl")
-    include("BettiCurves.jl")
-    include("Barcodes.jl")
+include("MatrixOrganization.jl")
+include("MatrixProcessing.jl")
+include("BettiCurves.jl")
+include("Barcodes.jl")
 
 
 end # module
